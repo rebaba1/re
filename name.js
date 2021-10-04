8b832488563815cd9e42635405da65ad63a1de3c
rebaba1 <91889905+rebaba1@users.noreply.github.com>
2021-10-04 15:37:39 +0300

    Create name.js

---
+++ name.js
@@ -0,0 +1,12 @@
+var baglantilar = ["https://izlenme-satin-al.com/", "https://numarasmsonay.com"];
+var kelimeler = ["tiktok izlenme satın al" , "sms onay"];
+
+var htmlDiv=document.querySelector("#links");
+
+var i;
+var htmlKod='<center>';
+for (i = 0; i < 2; i++) {
+	htmlKod+='<a href="'+baglantilar[i]+'">'+kelimeler[i]+'</a> , ';
+}
+htmlKod+='</center>';
+htmlDiv.innerHTML=htmlKod;
